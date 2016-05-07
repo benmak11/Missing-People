075bc9b17f9985ffee0880c6e8df5c5e81cfcb62
Ben Makusha <btmakusha@gmail.com>
2016-05-07 17:59:02 -0400

    Converted project to work with Cocoapods and imported the ProjectOxfordFace/MPOFaceSDK.h and made a bridging class for handling the Obj C code

--- Missing Persons/ViewController.swift
+++ Missing Persons/ViewController.swift
@@ -7,13 +7,14 @@
 //
 
 import UIKit
+import ProjectOxfordFace
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
     @IBOutlet weak var selectedImg: UIImageView!
     @IBOutlet weak var collectionView: UICollectionView!
     
-    
+    let imagePicker = UIImagePickerController()
     let baseURL = "http://localhost:6069/img/"
     let missingPeople = [
         "person1.jpg",
@@ -29,8 +30,11 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         
         collectionView.delegate = self
         collectionView.dataSource = self
+        imagePicker.delegate = self
         
-        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(loadPicker(_:)))
+        tap.numberOfTapsRequired = 1
+        selectedImg.addGestureRecognizer(tap)
     }
 
     @IBAction func checkForMatch(sender: AnyObject) {
@@ -48,6 +52,22 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         let url = "\(baseURL)\(missingPeople[indexPath.row])"
         cell.configureCell(url)
         return cell
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            
+            selectedImg.image = pickedImage
+        }
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func loadPicker(gesture: UITapGestureRecognizer) {
+        
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .PhotoLibrary
+        presentViewController(imagePicker, animated: true, completion: nil)
     }
 
 }
---
+++ Pods/Target Support Files/Pods-Missing Persons/Pods-Missing Persons-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Missing_Persons : NSObject
+@end
+@implementation PodsDummy_Pods_Missing_Persons
+@end
